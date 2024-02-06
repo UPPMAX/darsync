19c9327da443609f0cdb8ad3a09b13e95c1f00a2
dahlo <m.dahlo@gmail.com>
2024-02-06 15:42:24 +0100

    Updated name of default ssh key

--- dardel_ssh-keygen.sh
+++ dardel_ssh-keygen.sh
@@ -5,13 +5,13 @@ key_changed=false
 config_changed=false
 
 # Check if the ssh key file already exists
-while [[ -f ~/.ssh/id-ed25519-pdc ]]; do
+while [[ -f ~/.ssh/id_ed25519-pdc ]]; do
     read -p "The ssh key file already exists. Do you want to overwrite it (o), continue without changing it (c), or delete it (d)? (o/c/d): " option
 
     # If the user wants to overwrite the file
     if [[ $option == "o" ]]; then
         echo "Removing old ssh key."
-        rm ~/.ssh/id-ed25519-pdc
+        rm ~/.ssh/id_ed25519-pdc
         key_changed=true
         break
 
@@ -23,15 +23,15 @@ while [[ -f ~/.ssh/id-ed25519-pdc ]]; do
     # If the user wants to add a password
     elif [[ $option == "d" ]]; then
         echo "Deleting the ssh key."
-        rm ~/.ssh/id-ed25519-pdc
+        rm ~/.ssh/id_ed25519-pdc
         key_deleted=true
         break
     fi
 done
 
 # Create the ssh key without a password unless it should be deleted
-if [[ ! -f ~/.ssh/id-ed25519-pdc ]] && [[ $option != "d" ]]; then
-    ssh-keygen -q -t ed25519 -f ~/.ssh/id-ed25519-pdc -N ""
+if [[ ! -f ~/.ssh/id_ed25519-pdc ]] && [[ $option != "d" ]]; then
+    ssh-keygen -q -t ed25519 -f ~/.ssh/id_ed25519-pdc -N ""
     key_changed=true
 fi
 
